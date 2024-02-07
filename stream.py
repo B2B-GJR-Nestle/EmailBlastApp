24ac834c18390f9881c8abbed5eb163cbf82f4de
B2B-GJR-Nestle <158399203+B2B-GJR-Nestle@users.noreply.github.com>
2024-02-07 11:56:15 +0700

    Test to send in pdf

--- stream.py
+++ stream.py
@@ -9,38 +9,35 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import tempfile
 from PIL import Image
+from docx2pdf import convert
 
-class SessionState:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+img = Image.open('Nestle_Logo.png')
+st.set_page_config(page_title='B2B Email Blast App', page_icon=img)
+st.title("📑B2B GJR Email Blast Application")
+
+hide_st = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            <style>
+            """
+st.markdown(hide_st, unsafe_allow_html=True)
 
 def generate_document(template, output_path, data):
     doc = DocxTemplate(template)
     doc.render(data)
     doc.save(output_path)
 
-def download_template_from_github(repo_url, template_path):
-    template_url = f"{repo_url}/blob/main/{template_path}"
-    response = requests.get(template_url)
-    if response.status_code == 200:
-        template_content = response.content
-        return template_content
-    else:
-        st.error(f"Failed to download template from GitHub. Please check the URL: {template_url}")
-
-def send_email(subject, body, to_address, attachment_path, gmail_user, gmail_password, output_update_function):
+def send_email(subject, body, to_address, attachment_path, gmail_user, gmail_password):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = gmail_user
     msg['To'] = to_address
-
     with open(attachment_path, "rb") as attachment:
         part = MIMEApplication(attachment.read(), Name=os.path.basename(attachment_path))
         part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
         msg.attach(part)
-
     msg.attach(MIMEText(body, 'plain'))
-
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(gmail_user, gmail_password)
@@ -50,14 +47,12 @@ def update_excel_status(df, email, status):
     df.loc[df['Email'] == email, 'STATUS'] = status
     return df
 
-def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path, body_text, output_update_function, feature):
+def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path, body_text, feature):
     output_directory = 'Promotion'
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-
     placeholder = st.empty()
     placeholder.dataframe(excel_data)
-
     for index, row in excel_data.iterrows():
         # Skip rows where the 'Email' column is empty
         if pd.isnull(row['Email']):
@@ -68,7 +63,7 @@ def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path,
             'Salutation': row['Salutation'],
             'CompanyName': row['Company Name'],
         }
-
+        
         if feature == "Promotion":
             template = template_path
         else:
@@ -79,38 +74,31 @@ def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path,
                 st.error(f"No template found for product: {product}")
                 continue
 
-        output_filename = f"{output_directory} Program Feeding {row['Company Name']}.docx"
-        subject = f"Proposal Penawaran Kerjasama PT Nestle Indonesia & {row['Company Name']} ({product})"
-        generate_document(template, output_filename, merge_data)
+        output_filename_docx = f"{output_directory} Program Feeding {row['Company Name']}.docx"
+        output_filename_pdf = f"{output_directory} Program Feeding {row['Company Name']}.pdf"
+        generate_document(template, output_filename_docx, merge_data)
+        convert(output_filename_docx, output_filename_pdf)  # Convert to PDF
 
         # Use the provided body_text or a default if none is provided
+        subject = f"Proposal Penawaran Kerjasama PT Nestle Indonesia & {row['Company Name']} ({product})"
         email_body = body_text
-        send_email(subject, email_body, row['Email'], output_filename, gmail_user, gmail_password, output_update_function)
+        send_email(subject, email_body, row['Email'], output_filename_pdf, gmail_user, gmail_password)
 
         excel_data = update_excel_status(excel_data, row['Email'], 'Sent')
         placeholder.dataframe(excel_data)
 
-# Initialize SessionState
-session_state = SessionState(excel_data=None, template_dict={}, template_path=None)
-
 # Streamlit app
-st.title("📑B2B GJR Email Blast Application")
-
-hide_st = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st, unsafe_allow_html=True)
-
 # Upload Excel or CSV file
 excel_file = st.file_uploader("Upload Excel/CSV File", type=["xlsx", "csv"])
 if excel_file:
+    # Use pandas to read either Excel or CSV based on file extension
     if excel_file.name.endswith('.xlsx'):
-        session_state.excel_data = pd.read_excel(excel_file)
+        excel_data = pd.read_excel(excel_file)
     elif excel_file.name.endswith('.csv'):
-        session_state.excel_data = pd.read_csv(excel_file)
+        excel_data = pd.read_csv(excel_file)
+else:
+    st.warning('Please Upload Your Database File', icon="⚠️")
+    #excel_data = pd.read_excel("C:/Users/ASUS/Downloads/SalesProj/DATABASE.xlsx")  # Default path
 
 # Select feature: Proposal or Promotion
 st.write(f"## Blast Features")
@@ -120,13 +108,12 @@ feature = st.selectbox("Select Feature", ["Proposal", "Promotion"])
 template_path = None
 if feature == "Promotion":
     st.write(f"## 🎁 Promotional File")
-    template_path = st.file_uploader("Upload Promotion Template", type=["png", "jpg", "jpeg", "pdf", "docx"])
-    session_state.template_path = template_path
+    template_path = st.file_uploader("Upload Promotion Template", type=["docx"])
 
 # Upload Word document templates for Proposal feature
+template_dict = {}
+products = ["BearBrand", "Nescafe", "Milo"]
 if feature == "Proposal":
-    session_state.template_dict = {}
-    products = ["BearBrand", "Nescafe", "Milo"]
     for product in products:
         if product == "BearBrand":    
             st.write(f"## 🥛{product}")
@@ -140,21 +127,15 @@ if feature == "Proposal":
             # Save the uploaded template to a temporary file
             with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as temp_template:
                 temp_template.write(template_path.read())
-                session_state.template_dict[product] = temp_template.name
+                template_dict[product] = temp_template.name
 
 # Input for email body text
 default = """Salam,
-
 Semoga Bapak/Ibu keadaan baik. Saya mewakili tim PT. Nestlé Indonesia dengan senang hati ingin berbicara tentang peluang kerjasama program feeding karyawan yang dapat memberikan nilai tambah bagi perusahaan Anda.
-
 Sebagai salah satu perusahaan makanan dan minuman yang memiliki komitmen tinggi terhadap kualitas dan kesejahteraan, kami ingin menjalin kolaborasi dengan perusahaan Anda. Keunggulan kerjasama ini meliputi kontinuitas pasokan produk kami yang andal, serta diskon khusus sebagai bentuk apresiasi atas kerjasama yang baik.
-
 Untuk informasi lebih lanjut seputar produk listing dan harga, Anda dapat menemukannya dalam dokumen yang saya lampirkan. Kami sangat terbuka untuk berdiskusi lebih lanjut atau menjawab pertanyaan yang mungkin Anda miliki.
-
 Terima kasih banyak untuk waktu dan perhatiannya. Kami berharap dapat menjalin kerjasama yang baik dan saling menguntungkan.
-
 Salam,
-
 Bimo Agung Laksono
 B2B Executive, Greater Jakarta Region - PT Nestlé Indonesia
 Phone: +6287776162577
@@ -163,4 +144,4 @@ Mail : Bimoagung27@gmail.com
 body_text = st.text_area("Enter Email Body Text", default)
 
 if st.button("Execute Mail Merge"):
-    merge_and_send_emails(session_state.excel_data, "b2b.gjr.nestle@gmail.com", "alks kzuv wczc efch", session_state.template_path, body_text, st.empty(), feature)
+    merge_and_send_emails(excel_data, "b2b.gjr.nestle@gmail.com", "alks kzuv wczc efch", template_path, body_text, feature)
