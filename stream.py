d18b3975aa537b326996e7df6ac1ec8efae42c8a
B2B-GJR-Nestle <158399203+B2B-GJR-Nestle@users.noreply.github.com>
2024-02-13 16:34:44 +0700

    simple signature

--- stream.py
+++ stream.py
@@ -9,11 +9,9 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import tempfile
 from PIL import Image
-
 img = Image.open('Nestle_Logo.png')
 st.set_page_config(page_title='B2B Email Blast App', page_icon=img)
 st.title("📑B2B GJR Email Blast Application")
-
 hide_st = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -21,12 +19,10 @@ hide_st = """
             <style>
             """
 st.markdown(hide_st, unsafe_allow_html=True)
-
 def generate_document(template, output_path, data):
     doc = DocxTemplate(template)
     doc.render(data)
     doc.save(output_path)
-
 def download_template_from_github(repo_url, template_path):
     template_url = f"{repo_url}/blob/main/{template_path}"
     response = requests.get(template_url)
@@ -35,31 +31,23 @@ def download_template_from_github(repo_url, template_path):
         return template_content
     else:
         st.error(f"Failed to download template from GitHub. Please check the URL: {template_url}")
-
-def send_email(subject, body, html_body, to_address, attachment_path, gmail_user, gmail_password, output_update_function):
+def send_email(subject, body, to_address, attachment_path, gmail_user, gmail_password, output_update_function):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = gmail_user
     msg['To'] = to_address
-    
-    # Attach both plaintext and HTML versions of the email body
-    msg.attach(MIMEText(body, 'plain'))
-    msg.attach(MIMEText(html_body, 'html'))
-
     with open(attachment_path, "rb") as attachment:
         part = MIMEApplication(attachment.read(), Name=os.path.basename(attachment_path))
         part['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
         msg.attach(part)
-        
+    msg.attach(MIMEText(body, 'plain'))
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(gmail_user, gmail_password)
         server.sendmail(gmail_user, to_address, msg.as_string())
-
 def update_excel_status(df, email, status):
     df.loc[df['Email'] == email, 'STATUS'] = status
     return df
-
 def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path, body_text, subject_text, output_update_function, feature):
     output_directory = 'Promotion'
     if not os.path.exists(output_directory):
@@ -90,19 +78,9 @@ def merge_and_send_emails(excel_data, gmail_user, gmail_password, template_path,
         generate_document(template, output_filename, merge_data)
         # Use the provided body_text or a default if none is provided
         email_body = body_text.format(CompanyName=row['Company Name'])
-        html_email_body ="""
-<div style="text-align:left;">
-    <b>Salam,</b><br>
-    <span style="font-family:Arial; font-size:11pt; color:#645440; font-weight:bold;">Bimo Agung Laksono</span><br>
-    <span style="font-family:Arial; font-size:10pt;"><i>B2B Executive, Greater Jakarta Region - PT Nestlé Indonesia<i></span><br>
-    <span style="font-family:Arial; font-size:10pt;">P:<a href="tel:+6287776162577">+6287776162577 | Mail : <a href="mailto:Bimoagung27@gmail.com">Bimoagung27@gmail.com</a></span><br>
-    <img src="https://github.com/B2B-GJR-Nestle/EmailBlastApp/blob/main/Nestle_Signature.png" alt="Signature Image" style="width:100px;height:100px;">
-</div>
-"""
-        send_email(subject, email_body, html_email_body, row['Email'], output_filename, gmail_user, gmail_password, output_update_function)
+        send_email(subject, email_body, row['Email'], output_filename, gmail_user, gmail_password, output_update_function)
         excel_data = update_excel_status(excel_data, row['Email'], 'Sent')
         placeholder.dataframe(excel_data)
-
 # Streamlit app
 # Upload Excel or CSV file
 excel_file = st.file_uploader("Upload Excel/CSV File", type=["xlsx", "csv"])
@@ -115,11 +93,9 @@ if excel_file:
 else:
     st.warning('Please Upload Your Database File', icon="⚠️")
     #excel_data = pd.read_excel("C:/Users/ASUS/Downloads/SalesProj/DATABASE.xlsx")  # Default path
-
 # Select feature: Proposal or Promotion
 st.write(f"## Blast Features")
 feature = st.selectbox("Select Feature", ["Proposal", "Promotion"])
-
 # Upload Word document template
 template_path = None
 if feature == "Promotion":
@@ -128,6 +104,7 @@ if feature == "Promotion":
 
 # Upload Word document templates for Proposal feature
 template_dict = {}
+products = ["BearBrand", "Nescafe", "Milo"]
 products = ["General", "BearBrand", "Nescafe"]  # Change the product sequence
 if feature == "Proposal":
     for product in products:
@@ -136,18 +113,21 @@ if feature == "Proposal":
         elif product == "Nescafe":    
             st.write(f"## ☕ {product}")
         else:
+        if product == "General":
             st.write(f"## 🍫 {product}")
+        elif product == "BearBrand":
+            st.write(f"## 🥛 {product}")
+        else:
+            st.write(f"## ☕ {product}")
         template_path = st.file_uploader(f"Upload {product} Template", type=["docx","pdf"])
         if template_path:
             # Save the uploaded template to a temporary file
             with tempfile.NamedTemporaryFile(delete=False, suffix=".docx") as temp_template:
                 temp_template.write(template_path.read())
                 template_dict[product] = temp_template.name
-
 # Input for email subject text
 default_subject = "Proposal Penawaran Kerjasama PT Nestle Indonesia & {company_name}"
 subject_text = st.text_input("Enter Email Subject", default_subject)
-
 # Input for email body text
 default_body = """Yth. Bapak/Ibu
 {CompanyName},
@@ -155,14 +135,16 @@ di Tempat
 
 Semoga Bapak/Ibu keadaan baik. Saya mewakili tim PT. Nestlé Indonesia dengan senang hati ingin berbicara tentang peluang kerjasama program feeding karyawan yang dapat memberikan nilai tambah bagi perusahaan Anda.
 
-Sebagai salah satu perusahaan makanan dan minuman yang memiliki komitmen tinggi terhadap kualitas dan kesejahteraan, <i>kami ingin menjalin kolaborasi dengan perusahaan Anda</i>. Keunggulan kerjasama ini meliputi kontinuitas pasokan produk kami yang andal, serta diskon khusus sebagai bentuk apresiasi atas kerjasama yang baik.
+Sebagai salah satu perusahaan makanan dan minuman yang memiliki komitmen tinggi terhadap kualitas dan kesejahteraan, kami ingin menjalin kolaborasi dengan perusahaan Anda. Keunggulan kerjasama ini meliputi kontinuitas pasokan produk kami yang andal, serta diskon khusus sebagai bentuk apresiasi atas kerjasama yang baik.
 
 Untuk informasi lebih lanjut seputar produk listing dan harga, Anda dapat menemukannya dalam dokumen yang saya lampirkan. Kami sangat terbuka untuk berdiskusi lebih lanjut atau menjawab pertanyaan yang mungkin Anda miliki.
 
 Terima kasih banyak untuk waktu dan perhatiannya. Kami berharap dapat menjalin kerjasama yang baik dan saling menguntungkan.
-"""
 
+Salam,
+*Bimo Agung Laksono*
+_B2B Executive, Greater Jakarta Region - PT Nestlé Indonesia_
+Phone: +6287776162577 | Mail : Bimoagung27@gmail.com"""
 body_text = st.text_area("Enter Email Body Text", default_body, height=300)
-
 if st.button("Execute Mail Merge"):
     merge_and_send_emails(excel_data, "b2b.gjr.nestle@gmail.com", "alks kzuv wczc efch", template_path, body_text, subject_text, st.empty(), feature)
